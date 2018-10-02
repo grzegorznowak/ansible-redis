c778f7a9f8e9fd90ca0585fbb4a8830c032e8e95
Grzegorz Nowak <strange3studio@gmail.com>
2018-10-02 06:48:56 +0200

    extra round of tests to cover some of basic redis cases, fix spific redis configuration's path writability

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -41,3 +41,32 @@ def test_disabled_svc(host, svc):
 ])
 def test_ports(host, port):
     assert host.socket("tcp://127.0.0.1:{}".format(port)).is_listening
+
+
+@pytest.mark.parametrize('port', [
+    '6379', '6380'
+])
+def test_persistence(host, port):
+
+    # @see https://stackoverflow.com/a/7021653
+    cmd = host.run('{ echo "SAVE";  sleep 1; } | telnet 127.0.0.1 ' + port)
+    assert "OK" in cmd.stdout
+    assert "ERR" not in cmd.stdout
+
+
+@pytest.mark.parametrize('port', [
+    '6379', '6380'
+])
+def test_store(host, port):
+
+    cmd = host.run(
+        '{ echo "SET testkey \"TEST\"";  sleep 1; } | telnet 127.0.0.1 '
+        + port)
+    assert "OK" in cmd.stdout
+    assert "ERR" not in cmd.stdout
+
+    cmd = host.run(
+        '{ echo "GET testkey";  sleep 1; } | telnet 127.0.0.1 '
+        + port)
+    assert "TEST" in cmd.stdout
+    assert "ERR" not in cmd.stdout
--- molecule/docker_dev/tests/test_default.py
+++ molecule/docker_dev/tests/test_default.py
@@ -16,8 +16,57 @@ def test_pkg(host, pkg):
     assert package.is_installed
 
 
+@pytest.mark.parametrize('svc', [
+    'redis-cache0', 'redis-cache1'
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize('svc', [
+    'redis-server'
+])
+def test_disabled_svc(host, svc):
+    service = host.service(svc)
+
+    assert not service.is_running
+    assert not service.is_enabled
+
+
 @pytest.mark.parametrize('port', [
     '6379', '6380'
 ])
 def test_ports(host, port):
     assert host.socket("tcp://127.0.0.1:{}".format(port)).is_listening
+
+
+@pytest.mark.parametrize('port', [
+    '6379', '6380'
+])
+def test_persistence(host, port):
+
+    # @see https://stackoverflow.com/a/7021653
+    cmd = host.run('{ echo "SAVE";  sleep 1; } | telnet 127.0.0.1 ' + port)
+    assert "OK" in cmd.stdout
+    assert "ERR" not in cmd.stdout
+
+
+@pytest.mark.parametrize('port', [
+    '6379', '6380'
+])
+def test_store(host, port):
+
+    cmd = host.run(
+        '{ echo "SET testkey \"TEST\"";  sleep 1; } | telnet 127.0.0.1 '
+        + port)
+    assert "OK" in cmd.stdout
+    assert "ERR" not in cmd.stdout
+
+    cmd = host.run(
+        '{ echo "GET testkey";  sleep 1; } | telnet 127.0.0.1 '
+        + port)
+    assert "TEST" in cmd.stdout
+    assert "ERR" not in cmd.stdout
